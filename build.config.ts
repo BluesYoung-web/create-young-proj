71e9bb30bb85da35362e7a8b7484c8de54f84687
BluesYoung-web <15171255945@163.com>
2022-12-03 16:47:57 +0800

    chore: :hammer: add build config

---
+++ build.config.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: zhangyang
+ * @Date: 2022-12-03 16:46:21
+ * @LastEditTime: 2022-12-03 16:46:53
+ * @Description:
+ */
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  entries: ['src/index'],
+  clean: true,
+  rollup: {
+    inlineDependencies: true,
+    esbuild: {
+      minify: true,
+    },
+  },
+  alias: {
+    // we can always use non-transpiled code since we support 14.18.0+
+    prompts: 'prompts/lib/index.js',
+  },
+});
