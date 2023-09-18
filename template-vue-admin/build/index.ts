e5b23f9a63af462e2ea1e52af985e091f6cb3990
张扬 <bluesyoung_web@163.com>
2023-09-18 14:07:15 +0800

    chore: 🏡 删除废弃模板

--- template-vue-admin/build/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: zhangyang
- * @Date: 2023-01-10 14:46:42
- * @LastEditTime: 2023-01-10 14:46:43
- * @Description:
- */
-export * from './plugins';
--- template-vue-admin/src/stores/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: zhangyang
- * @Date: 2022-03-01 14:42:57
- * @LastEditTime: 2023-01-09 09:19:05
- * @Description:
- */
-export * from './tags';
-export * from './session';
-export * from './local';
