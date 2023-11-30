f662f94d402df8ac3b6fea135bc1043fbc707345
张扬 <bluesyoung_web@163.com>
2023-11-30 11:04:40 +0800

    chore: 🏡 后台模板兼容配置调整

--- template-nuxt-admin/nuxt.config.ts
+++ template-nuxt-admin/nuxt.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyang
  * @Date: 2023-09-21 15:57:55
- * @LastEditTime: 2023-11-15 11:52:33
+ * @LastEditTime: 2023-11-30 11:03:49
  * @Description:
  */
 import { resolve } from 'node:path'
@@ -84,11 +84,10 @@ export default defineNuxtConfig({
 
   vite: {
     build: {
-      // 兼容钉钉浏览器
-      target: 'es2015',
       sourcemap: false,
-      /* hack fix 360 极速浏览器 13.5 不支持 inset */
-      cssTarget: 'chrome61',
+      // 兼容旧浏览器
+      target: ['chrome58'],
+      cssTarget: ['chrome58'],
     },
     css: {
       preprocessorOptions: {
