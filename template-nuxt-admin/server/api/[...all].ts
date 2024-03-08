dc4abb27bf9d3b67763bb3b98044faa4f88fe72a
张扬 <bluesyoung_web@163.com>
2024-03-08 15:07:14 +0800

    chore!: 🏡 change proxy method

--- template-nuxt-admin/server/api/[...all].ts
+++ template-nuxt-admin/server/api/[...all].ts
@@ -1,10 +1,14 @@
 /*
  * @Author: zhangyang
  * @Date: 2023-07-21 09:50:36
- * @LastEditTime: 2023-07-21 09:50:36
+ * @LastEditTime: 2024-03-08 15:02:22
  * @Description:
  */
+import { createProxyEventHandler } from 'h3-proxy'
+
 export default eventHandler(async (event) => {
-  const proxy = createTransparentProxy(process.env.NUXT_PUBLIC_API_BASE as string)
-  await proxy.handle(event)
+  const proxy = createProxyEventHandler({
+    target: process.env.NUXT_PUBLIC_API_BASE as string,
+  })
+  return proxy(event)
 })
--- template-nuxt-website/server/utils/proxy.ts
+++ template-nuxt-website/server/utils/proxy.ts
@@ -1,39 +1,10 @@
 /*
  * @Author: zhangyang
  * @Date: 2023-09-27 11:22:08
- * @LastEditTime: 2023-11-01 19:10:45
+ * @LastEditTime: 2024-03-08 15:05:53
  * @Description:
  */
-import type { ServerOptions as HTTPProxyOptions } from 'http-proxy'
-import ProxyModule from 'http-proxy'
 import type { H3Event } from 'h3'
-
-/**
- * @param target 接口代理的具体地址
- * @param defaults 代理配置
- * @example
- * export default defineEventHandler(async (event) => {
- *   const proxy = createTransparentProxy(`http://127.0.0.1:3333`);
- *   await proxy.handle(event);
- * });
- */
-export function createTransparentProxy(target: string, defaults: HTTPProxyOptions = {}) {
-  const proxy = ProxyModule.createProxy()
-  const handle = (event: H3Event, opts: HTTPProxyOptions = {}) => {
-    return new Promise<void>((resolve, reject) => {
-      proxy.web(event.node.req, event.node.res, { target, ...defaults, ...opts }, (error: any) => {
-        if (error.code !== 'ECONNRESET')
-          reject(error)
-
-        resolve()
-      })
-    })
-  }
-  return {
-    proxy,
-    handle,
-  }
-}
 
 /**
  * 服务端发送的请求
